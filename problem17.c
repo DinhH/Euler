89c43fb968ff36174afb086725a124975f079480
dinhh <dinh4512@cs.utexas.edu>
2015-01-10 18:57:55 -0600

    still fixing on bug for #17

--- problem17.c
+++ problem17.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-#include <stdbib.h>
 
-int void main()
+#define FIRST (3 + 3 + 5 + 4 + 4 + 3 + 5 + 5 + 4)
+#define SECOND (3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8)
+#define THIRD (6 + 6 + 5 + 5 + 5 + 7 + 6 + 6)
+#define COUNT 7
+#define ADD 3
+
+int main(void)
 {
+	int sum = 0;
+
+	sum += 9 * FIRST;
+	sum += SECOND;
+	sum += 10 * THIRD;	
+	
+	sum *= 10;
+	
+	sum += 100 * FIRST;
+	
+	sum += 900 * COUNT;
+	sum += 890 * ADD + 1;
+	
+	sum += 12;
+	
+	printf("%d\n", sum);
+	return 0;
+}
 
