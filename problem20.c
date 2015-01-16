3dd12f7f17e939c0b052689dc8c0920afa3d151a
dinhh <dinh4512@cs.utexas.edu>
2015-01-16 11:29:05 -0600

    still have problem with <gmp.h>

--- problem20.c
+++ problem20.c
@@ -1,4 +1,30 @@
 //Find the sum of the digits in the number 100!
 
-#define <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
 
+int main(void)
+{
+	mpz_t count;
+	mpz_init(count);
+
+	char *str;
+	int i;
+	int ret;
+	
+	mpz_fac_ui(count, 100);
+	str = mpz_get_str(NULL,10, count);
+
+	for (i = 0; str[i]; i++)
+	{
+		ret += str[i] - '0';
+	}
+	printf("%d\n", ret);
+
+	free(str);
+	mpz_clear(count);
+
+	return 0;
+}
+	
