104b164a55a36a3f7b5b9464234f8b63dba0bed1
dinhh <dinh4512@cs.utexas.edu>
2015-01-24 10:16:21 -0600

    finished #21 now debugging

--- problem21.c
+++ problem21.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
-
+static int divisors(int n);
 int main(void)
 {
 	int i, x;
-	for(x = 0; x <= 100; x++)
+	for(i = 2; i <= 1000; i++)
 	{
-		return i++;
+		x = divisors(i);
+		if (i < x && i == divisors(x))
+		{
+			int sum += i + x;
+		}
 	}
+	
+	printf("%d\n", sum);
 
 	return 0;
 }
+
+int divisor(int n)
+{
+	int i;
+	int k =n;
+
+	for (i = 2; i <= k; i++)
+	{
+		int y =1;
+		while (k % i ==0)
+		{
+			y *= i;
+			k /= i;
+		}
+	int sum *= (y*i - 1) / (i -1);
+	}
+	
+	return sum - n ;
+}
+
 
