4caa30d6242b1c69bb8ad988898c33fbefb77ea0
dinhh <dinh4512@cs.utexas.edu>
2015-01-25 09:42:36 -0600

    fixed #21 bug

--- problem21.c
+++ problem21.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 static int divisors(int n);
 int main(void)
 {
 	int i, x;
+	int sum = 0;
+
 	for(i = 2; i <= 1000; i++)
 	{
 		x = divisors(i);
 		if (i < x && i == divisors(x))
 		{
-			int sum += i + x;
+			sum += i + x;
 		}
 	}
 	
@@ -20,7 +23,8 @@ int main(void)
 int divisor(int n)
 {
 	int i;
-	int k =n;
+	int k = n;
+	int sum = 0;	
 
 	for (i = 2; i <= k; i++)
 	{
@@ -30,7 +34,7 @@ int divisor(int n)
 			y *= i;
 			k /= i;
 		}
-	int sum *= (y*i - 1) / (i -1);
+	sum *= (y*i - 1) / (i -1);
 	}
 	
 	return sum - n ;
