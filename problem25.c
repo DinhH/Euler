f4bd5ff7033d459ad9d5fa2be0b853bd99f1642f
dinhh <dinh4512@cs.utexas.edu>
2015-01-28 10:53:31 -0600

    finished with 25 but still one bug?

--- problem25.c
+++ problem25.c
@@ -1,8 +1,35 @@
-#include <studio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <gmp.h>
 
 int main (void)
 {
+	mpz_t n;
+	mpz_init(n);
 	
+	int i = 1;
+	
+	for (i = 1;; i++)
+	{
+		char *str;
+		size_t length;
+	
+	mpz_fib_ui(n,i);
+	str = mpz_get_str(NULL, 10,n);
+	length = strlen(str);
+
+	free(str);
+
+	if (length == 1000)
+	{
+		printf("%d\n", i);
+		break;
+	}
+
+	mpz_clear(n);
+
+	return 0;
+}
+
+		
