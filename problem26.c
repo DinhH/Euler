fc80b5fb0fc50e61653cccbd4b16e2c3650b0d7f
dinhh <dinh4512@cs.utexas.edu>
2015-02-02 11:09:31 -0600

    worked on algorithm for #26

--- problem26.c
+++ problem26.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <gmp.h>
 
 int main(void)
 {
-	int i;
+	mpz_t i, j;
+	mpz_init(i);
+	mpz_init(j);
+
+	unsigned max_length = 0;
+	unsigned max_size = 0;
+	
+	unsigned x;
+
+	for (x = 2; x < i; x++)
+	{
+		unsigned temp = 1;
+		
+		if( x % 2 == 0 || x % 5 == 0) 
+		{
+			continue;
+		}
+	}
 
 }
 
